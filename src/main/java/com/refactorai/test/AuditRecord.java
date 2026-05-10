8f391879a807939a1e30e6501a58aeaed0413d8b
The-Aziz <191167080+The-Aziz@users.noreply.github.com>
2026-05-10 22:08:26 +0100

    Add AuditRecord data class

---
+++ src/main/java/com/refactorai/test/AuditRecord.java
@@ -0,0 +1,12 @@
+package com.refactorai.test;
+
+import java.util.Date;
+
+public class AuditRecord {
+    public String action;
+    public String userId;
+    public Date timestamp;
+    public String details;
+    public String ipAddress;
+    public String sessionId;
+}
