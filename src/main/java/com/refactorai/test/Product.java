e8b3bbbfb152918644726770a27f29e55108b5f1
The-Aziz <191167080+The-Aziz@users.noreply.github.com>
2026-05-10 22:08:24 +0100

    Add Product data class

---
+++ src/main/java/com/refactorai/test/Product.java
@@ -0,0 +1,12 @@
+package com.refactorai.test;
+
+public class Product {
+    public String productId;
+    public String name;
+    public double price;
+    public int stock;
+    public String category;
+    public double weight;
+    public String description;
+    public boolean available;
+}
